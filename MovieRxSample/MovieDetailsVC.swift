65b49919a2be47ff9a304f8a54bf316dd51d15c8
Ahmed Ezzat Mohammed <ahmed.bedir@vodafone.com>
2023-06-06 02:40:10 +0300

    delete label outlet

--- MovieRxSample/MovieDetailsVC.swift
+++ MovieRxSample/MovieDetailsVC.swift
@@ -16,7 +16,6 @@ class MovieDetailsVC: UIViewController {
     @IBOutlet weak var moviePoster: UIImageView!
     @IBOutlet weak var lbl_Title: UILabel!
     @IBOutlet weak var lbl_ReleaseDate: UILabel!
-   // @IBOutlet weak var lbl_OverView: UILabel!
     @IBOutlet weak var lbl_OverView: UITextView!
     
     
