f7c0c7a25c292bc21f45f8f095b8fc8eda47ecb4
Ahmed Ezzat Mohammed <ahmed.bedir@vodafone.com>
2023-06-06 01:58:51 +0300

    change in Details UI

--- MovieRxSample/MovieDetailsVC.swift
+++ MovieRxSample/MovieDetailsVC.swift
@@ -14,14 +14,9 @@ import Kingfisher
 
 class MovieDetailsVC: UIViewController {
     @IBOutlet weak var moviePoster: UIImageView!
-    @IBOutlet weak var lbl_Avarage: UILabel!
-    
-    @IBOutlet weak var lbl_CountVote: UILabel!
-    
-    @IBOutlet weak var lbl_OriginalLang: UILabel!
-    @IBOutlet weak var lbl_RelaseDate: UILabel!
-    
-    @IBOutlet weak var lbl_Genre: UILabel!
+    @IBOutlet weak var lbl_Title: UILabel!
+    @IBOutlet weak var lbl_ReleaseDate: UILabel!
+    @IBOutlet weak var lbl_OverView: UILabel!
     
     
     
@@ -36,48 +31,27 @@ class MovieDetailsVC: UIViewController {
         
         
         movieDetailsViewModel.movieDetail.asObserver()
-                            .map({$0.voteAverage})
-                            .map({"\($0 ?? 0.0)"})
-                            .bind(to: lbl_Avarage.rx.text)
+                            .map({$0.title})
+                            .bind(to: lbl_Title.rx.text)
                             .disposed(by: disposeBag)
+        
+        movieDetailsViewModel.movieDetail.asObserver()
+                            .map({$0.overview})
+                            .bind(to: lbl_OverView.rx.text)
+                            .disposed(by: disposeBag)
+        
         
         movieDetailsViewModel.movieDetail.asObserver()
                             .map({$0.releaseDate})
-                            .bind(to: lbl_RelaseDate.rx.text)
+                            .bind(to: lbl_ReleaseDate.rx.text)
                             .disposed(by: disposeBag)
-        
-        movieDetailsViewModel.movieDetail.asObserver()
-                            .map({$0.originalLanguage})
-                            .bind(to: lbl_OriginalLang.rx.text)
-                            .disposed(by: disposeBag)
-        
-        movieDetailsViewModel.movieDetail.asObserver()
-                            .map({$0.voteCount})
-                            .map({"\($0 ?? 0)"})
-                            .bind(to: lbl_CountVote.rx.text)
-                            .disposed(by: disposeBag)
-        
-        
+
         movieDetailsViewModel.movieDetail.asObserver()
                             .map({$0.posterPath})
                             .map({URL.init(string: "https://image.tmdb.org/t/p/w500\($0 ?? "")")})
                             .bind(to: moviePoster.kf.rx.image())
                             .disposed(by: disposeBag)
-        
-        
-        
-        movieDetailsViewModel.movieDetail.asObserver()
-                        .map({$0.genres?.map({$0.name ?? ""}).joined(separator: ",") })
-                        .bind(to: lbl_Genre.rx.text)
-                        .disposed(by: disposeBag)
-        
 
-        
-        
-        
-        
-        
-        
     }
     
 
